fdede929b94b5534f28125c795671a53aaffe662
Moritz <moritz.mueller@coverbase.co>
2023-09-08 13:57:57 +0200

    feat(console): Add basic console page

---
+++ apps/console/nuxt.config.ts
@@ -0,0 +1,32 @@
+export default defineNuxtConfig({
+    modules: ["@unocss/nuxt"],
+
+    css: ["@unocss/reset/tailwind.css", "@coverbase/ui/style"],
+
+    unocss: {
+        rules: [["grid-layout-header", { "grid-template-rows": "auto 1fr auto" }]],
+        safelist: [
+            "overflow-y-scroll",
+            "overflow-x-hidden",
+            "bg-slate-50",
+            "font-sans",
+            "text-slate-900",
+            "text-base",
+        ],
+    },
+
+    app: {
+        head: {
+            title: "Coverbase",
+            bodyAttrs: {
+                class: "overflow-y-scroll overflow-x-hidden bg-slate-50 font-sans text-slate-900 text-sm",
+            },
+        },
+    },
+
+    $production: {
+        nitro: {
+            preset: "cloudflare-pages",
+        },
+    },
+});
